53ef7d54f1cafa5eb5801f875ad278e88c870780
deepak <deepak@13f79535-47bb-0310-9956-ffa450edef68>
2016-07-16 10:30:49 +0000

    (OFBIZ-6274) Renamed OFBiz artefacts from org.ofbiz.* to org.apache.ofbiz.*.Thanks to Taher for working on it.
    git-svn-id: https://svn.apache.org/repos/asf/ofbiz/trunk/applications/humanres@1752920 13f79535-47bb-0310-9956-ffa450edef68

--- src/main/java/org/apache/ofbiz/humanres/HumanResEvents.java
+++ src/main/java/org/apache/ofbiz/humanres/HumanResEvents.java
@@ -16,7 +16,7 @@
  * specific language governing permissions and limitations
  * under the License.
  *******************************************************************************/
-package org.ofbiz.humanres;
+package org.apache.ofbiz.humanres;
 
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -24,14 +24,14 @@ import java.util.List;
 import java.util.Map;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import org.ofbiz.base.util.Debug;
-import org.ofbiz.base.util.UtilValidate;
-import org.ofbiz.entity.Delegator;
-import org.ofbiz.entity.GenericEntityException;
-import org.ofbiz.entity.GenericValue;
-import org.ofbiz.entity.condition.EntityCondition;
-import org.ofbiz.entity.condition.EntityOperator;
-import org.ofbiz.entity.util.EntityQuery;
+import org.apache.ofbiz.base.util.Debug;
+import org.apache.ofbiz.base.util.UtilValidate;
+import org.apache.ofbiz.entity.Delegator;
+import org.apache.ofbiz.entity.GenericEntityException;
+import org.apache.ofbiz.entity.GenericValue;
+import org.apache.ofbiz.entity.condition.EntityCondition;
+import org.apache.ofbiz.entity.condition.EntityOperator;
+import org.apache.ofbiz.entity.util.EntityQuery;
 
 public class HumanResEvents {
     public static final String module = HumanResEvents.class.getName();
